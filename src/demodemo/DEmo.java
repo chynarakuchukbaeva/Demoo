7f2a2bfe4151a3ad1abda408b29c2f9fe03e7b44
iskanderuraiymov <iskanderuraiymov@192.168.1.64>
2018-12-27 16:43:51 -0600

    cleaned

--- src/demodemo/DEmo.java
+++ src/demodemo/DEmo.java
@@ -4,11 +4,11 @@ public class DEmo {
 	public static void main(String[] args) {
 		System.out.println("aijaaaaaan ");
 		System.out.println("ooooou");
-<<<<<<< HEAD
+
 		System.out.println("dsdasd");
-=======
+
 		System.out.println("its soo cool up here");
->>>>>>> branch 'master' of https://github.com/chynarakuchukbaeva/Demoo.git
+
 	}
 
 }
